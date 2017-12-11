4f8026f70c1290e108db1ae933a0416670ea0d21
Martyn Janes <martyn@obany.com>
2017-12-11 14:20:46 +0300

    Updated module paths

--- create-apps.sh
+++ create-apps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-node unitejs/cli/bin/unite configure --packageName=react-javascript --title="React JavaScript" --profile=ReactJavaScript --outputDirectory=./apps/react-javascript
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=react-javascript --title="React JavaScript" --profile=ReactJavaScript --outputDirectory=./apps/react-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-javascript
 
-node unitejs/cli/bin/unite configure --packageName=react-typescript-amd --title="React TypeScript CommonJS" --profile=ReactTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/react-typescript-commonjs
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=react-typescript-amd --title="React TypeScript CommonJS" --profile=ReactTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/react-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-typescript-commonjs
 
-node unitejs/cli/bin/unite configure --packageName=react-typescript-systemjs --title="React TypeScript SystemJS" --profile=ReactTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestEngine=ChromeHeadless --outputDirectory=./apps/react-typescript-systemjs
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=react-typescript-systemjs --title="React TypeScript SystemJS" --profile=ReactTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --unitTestRunner=Karma --unitTestEngine=ChromeHeadless --outputDirectory=./apps/react-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-typescript-systemjs
