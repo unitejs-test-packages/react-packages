8e957e578baeb89809876b50cedd36bc3379f8ee
Martyn Janes <martyn@obany.com>
2017-12-18 10:27:43 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=react-javascript --title="React JavaScript" --profile=ReactJavaScript --outputDirectory=./apps/react-javascript
-mkdir -p ./apps/react-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
-mkdir -p ./apps/react-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
-node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/react-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/react-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/react-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=react-javascript-commonjs --title="React JavaScript CommonJS" --profile=ReactJavaScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/react-javascript-commonjs
+mkdir -p ./apps/react-javascript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/react-javascript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/react-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/react-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/react-javascript-commonjs
+
+node unitejs/unitejs-cli/bin/unite configure --packageName=react-javascript-systemjs --title="React JavaScript SystemJS" --profile=ReactJavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/react-javascript-systemjs
+mkdir -p ./apps/react-javascript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/react-javascript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/react-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/react-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/react-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/react-javascript-systemjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=react-typescript-commonjs --title="React TypeScript CommonJS" --profile=ReactTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/react-typescript-commonjs
 mkdir -p ./apps/react-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
